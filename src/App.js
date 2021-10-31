d119ff934d5b0f813967075ca10eed3b5021bd50
Krish Udma <krishudma@gmail.com>
2021-10-31 13:21:30 +0400

    changes for Snack Circus v2.01

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logos.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Snack Circus" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="Snack Circus"
+          href="https://snackcircus.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h1>Concession Equipment's, Ingredients and Accessories</h1>
+          <h1>Snack Circus</h1>
+          <p>Concession Equipment's, Ingredients and Accessories</p>
         </a>
       </header>
     </div>
