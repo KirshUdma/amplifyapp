55776e82a2b2990448c9a4b1687ed5cb41cc0049
Krish Udma <krishudma@gmail.com>
2021-10-31 15:07:31 +0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -17,11 +18,10 @@ function App() {
         >
           snackcircus
         </a>
-        <p></p>
-
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
